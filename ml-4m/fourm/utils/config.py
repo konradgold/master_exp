1c7c0ef4f9a8e8446c43dc523f487658f45193ee
Konrad Goldenbaum <konrad.goldenbaum@outlook.de>
2025-11-12 17:27:43 +0100

    placed def args into config

--- ml-4m/fourm/utils/config.py
+++ ml-4m/fourm/utils/config.py
@@ -271,3 +271,236 @@ if __name__ == '__main__':
     cfg = Config(load=True)
     print(cfg.DATA)
     
+
+def get_args(args=None):
+    config_parser = parser = argparse.ArgumentParser(description='Generation Config', add_help=False)
+    parser.add_argument('-c', '--config', default='', type=str, metavar='FILE',
+                       help='YAML config file specifying default arguments')
+    parser.add_argument('-dc', '--data_config', default='', type=str, metavar='FILE',
+                    help='YAML config file specifying validation data specific arguments')
+    parser.add_argument('-gc', '--gen_config', default='', type=str, metavar='FILE',
+                    help='YAML config file specifying generation specific arguments')
+    parser.add_argument('-src', '--sr_config', default='', type=str, metavar='FILE',
+                    help='YAML config file specifying super resolution specific arguments')
+
+    parser = argparse.ArgumentParser('FourM generation script', add_help=False)    
+
+    parser.add_argument('--run_name', type=str, default='auto')
+
+    
+    # Generation parameters
+    parser.add_argument('--cond_domains', default='caption-det', type=str,
+                        help='Conditioning domain names, separated by hyphen (default: %(default)s)')
+    parser.add_argument('--target_domains', default='tok_clip-tok_normal-tok_rgb', type=str,
+                        help='Target domain names, separated by hyphen. (default: %(default)s)')
+    parser.add_argument('--tokens_per_target', default='196-196-196', type=str,
+                        help='Number of tokens for each target modality. (default: %(default)s)')
+    parser.add_argument('--autoregression_schemes', default='maskgit-maskgit-maskgit', type=str,
+                        help='Scheme of autoregressive generation for each target modality. "maskgit", "roar" or "autoregressive" (default: %(default)s)')
+    parser.add_argument('--decoding_steps', default='25-25-25', type=str,
+                        help='Number of decoding steps for each target modality. (default: %(default)s)')
+    parser.add_argument('--token_decoding_schedules', default='cosine-cosine-cosine', type=str,
+                        help='Token decoding schedules for each target modality. (default: %(default)s)')
+    parser.add_argument('--temps', default='5.0-1.0-1.0', type=str,
+                        help='Starting temperature for each target modality. (default: %(default)s)')
+    parser.add_argument('--temp_schedules', default='linear-linear-linear', type=str,
+                        help='Temperature schedules for each target modality. (default: %(default)s)')
+    parser.add_argument('--cfg_scales', default='4.0-4.0-4.0', type=str,
+                        help='Classifier-free guidance scales for each target modality. (default: %(default)s)')
+    parser.add_argument('--cfg_schedules', default='constant-constant-constant', type=str,
+                        help='Classifier-free guidance schedules for each target modality. (default: %(default)s)')
+    parser.add_argument('--cfg_grow_conditioning', action='store_true',
+                        help='After every completed modality, add them to classifier-free guidance conditioning.')
+    parser.add_argument('--no_cfg_grow_conditioning', action='store_false', dest='cfg_grow_conditioning',
+                        help='Perform classifier-free guidance only on initial conditioning.')
+    parser.set_defaults(cfg_grow_conditioning=True)
+    parser.add_argument('--top_p', default=0.0, type=float,
+                        help='top_p > 0.0: Keep the top tokens with cumulative probability >= top_p (a.k.a. nucleus filtering) (default: %(default)s)')
+    parser.add_argument('--top_k', default=0.0, type=float,
+                        help='top_k > 0: Keep only top k tokens with highest probability (a.k.a. top-k filtering) (default: %(default)s)')
+
+    # Super resolution parameters
+    parser.add_argument('--sr_cond_domains', default=None, type=str,
+                        help='SuperRes: Conditioning domain names, separated by hyphen. If none, all base conditions and targets are used. (default: %(default)s)')
+    parser.add_argument('--sr_target_domains', default='tok_clip@448-tok_rgb@448', type=str,
+                        help='SuperRes: Target domain names, separated by hyphen. (default: %(default)s)')
+    parser.add_argument('--sr_tokens_per_target', default='784', type=str,
+                        help='SuperRes: Number of tokens for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_autoregression_schemes', default='maskgit', type=str,
+                        help='SuperRes: Scheme of autoregressive generation for each target modality. "maskgit", "roar" or "autoregressive" (default: %(default)s)')
+    parser.add_argument('--sr_decoding_steps', default='8', type=str,
+                        help='SuperRes: Number of decoding steps for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_token_decoding_schedules', default='cosine', type=str,
+                        help='SuperRes: Token decoding schedules for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_temps', default='1.0', type=str,
+                        help='SuperRes: Starting temperature for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_temp_schedules', default='linear', type=str,
+                        help='SuperRes: Temperature schedules for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_cfg_scales', default='4.0', type=str,
+                        help='SuperRes: Classifier-free guidance scales for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_cfg_schedules', default='constant', type=str,
+                        help='SuperRes: Classifier-free guidance schedules for each target modality. (default: %(default)s)')
+    parser.add_argument('--sr_cfg_grow_conditioning', action='store_true',
+                        help='SuperRes: After every completed modality, add them to classifier-free guidance conditioning.')
+    parser.add_argument('--sr_no_cfg_grow_conditioning', action='store_false', dest='sr_cfg_grow_conditioning',
+                        help='SuperRes: Perform classifier-free guidance only on initial conditioning.')
+    parser.set_defaults(sr_cfg_grow_conditioning=True)
+    parser.add_argument('--sr_top_p', default=0.0, type=float,
+                        help='SuperRes: top_p > 0.0: Keep the top tokens with cumulative probability >= top_p (a.k.a. nucleus filtering) (default: %(default)s)')
+    parser.add_argument('--sr_top_k', default=0.0, type=float,
+                        help='SuperRes: top_k > 0: Keep only top k tokens with highest probability (a.k.a. top-k filtering) (default: %(default)s)')
+    
+    # Script parameters
+    parser.add_argument('--num_samples', default=None,
+                        help='Maximum number of samples to draw from the dataloader. (default: %(default)s)')
+    parser.add_argument('--num_variations', default=1, type=int,
+                        help='Number of variations to generate from each sample. (default: %(default)s)')
+    parser.add_argument('--seed', default=0, type=int, help='Random seed ')
+    
+    # Tokenizer settings
+    parser.add_argument('--detokenizer_steps', default=25, type=int,
+                        help='Number of DDPM/DDIM steps for decoding with diffusion-based tokenizers. (default: %(default)s)')
+    parser.add_argument('--rgb_tok_id', default=None, type=str,
+                        help='RGB tokenizer ID (default: %(default)s)')
+    parser.add_argument('--depth_tok_id', default=None, type=str,
+                        help='Depth tokenizer ID (default: %(default)s)')
+    parser.add_argument('--normal_tok_id', default=None, type=str,
+                        help='Normal tokenizer ID (default: %(default)s)')
+    parser.add_argument('--edges_tok_id', default=None, type=str,
+                        help='Edges tokenizer ID (default: %(default)s)')
+    parser.add_argument('--semseg_tok_id', default=None, type=str,
+                        help='Semseg tokenizer ID (default: %(default)s)')
+    parser.add_argument('--clip_tok_id', default=None, type=str,
+                        help='CLIP tokenizer ID (default: %(default)s)')
+    parser.add_argument('--dinov2_tok_id', default=None, type=str,
+                        help='DINOv2 tokenizer ID (default: %(default)s)')
+    parser.add_argument('--imagebind_tok_id', default=None, type=str,
+                        help='ImageBind tokenizer ID (default: %(default)s)')
+    parser.add_argument('--dinov2_glob_tok_id', default=None, type=str,
+                        help='DINOv2 global tokenizer ID (default: %(default)s)')
+    parser.add_argument('--imagebind_glob_tok_id', default=None, type=str,
+                        help='ImageBind global tokenizer ID (default: %(default)s)')
+    parser.add_argument('--sam_instance_tok_id', default=None, type=str,
+                        help='SAM instance tokenizer ID (default: %(default)s)')
+    parser.add_argument('--human_poses_tok_id', default=None, type=str,
+                        help='Human poses tokenizer ID (default: %(default)s)')
+    parser.add_argument('--text_tok_path', default='fourm/utils/tokenizer/trained/text_tokenizer_4m_wordpiece_30k.json', type=str,
+                        help='Text tokenizer path (default: %(default)s)')
+    
+    # ControlNet parameters
+    parser.add_argument('--activate_controlnet', action='store_true',
+                        help='When enabled, RGB detokenizer will be replaced by RGB ControlNet.')
+    parser.add_argument('--no_activate_controlnet', action='store_false', dest='activate_controlnet')
+    parser.set_defaults(activate_controlnet=False)
+    parser.add_argument('--controlnet_id', default=None, type=str,
+                        help='RGB ControlNet ID (default: %(default)s)')
+    parser.add_argument('--controlnet_guidance_scale', default=2.5, type=float,
+                        help='RGB ControlNet guidance scale (default: %(default)s)')
+    parser.add_argument('--controlnet_cond_scale', default=0.8, type=float,
+                        help='RGB ControlNet conditioning scale (default: %(default)s)')
+    
+    # Model parameters
+    parser.add_argument('--model', default=None, type=str, metavar='MODEL',
+                        help='4M model: Hugging Face Hub ID, or path to local safetensors checkpoint (default: %(default)s)')
+    parser.add_argument('--sr_model', default=None, type=str, metavar='MODEL',
+                        help='Superres model: Hugging Face Hub ID, or path to local safetensors checkpoint (default: %(default)s)')
+    parser.add_argument('--image_size', default=224, type=int,
+                        help='Image size. (default: %(default)s)')
+    parser.add_argument('--patch_size', default=16, type=int,
+                        help='Base patch size for image-like modalities (default: %(default)s)')
+    
+    parser.add_argument('--dtype', type=str, default='float32',
+                        choices=['float16', 'bfloat16', 'float32', 'bf16', 'fp16', 'fp32'],
+                        help='Data type (default: %(default)s')
+    # Data
+    parser.add_argument('--data_path', default='/mnt/datasets/cc12_multitask_224/val', 
+                        help='Path to dataset (default: %(default)s)')
+    parser.add_argument('--data_name', default='', type=str,
+                        help='Name of dataset, used for wandb and output folder. (default: %(default)s)')
+    parser.add_argument('--num_workers', default=1, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem', help='')
+    parser.set_defaults(pin_mem=True)
+    parser.add_argument('--parti_prompts_t5_embs', default=None, type=str,
+                        help="(Optional) path to pre-computed T5 embeddings for PartiPrompts (in .npz format)")
+    
+    # Misc.
+    parser.add_argument('--s3_endpoint', default='', type=str, help='S3 endpoint URL')
+    parser.add_argument('--s3_path', default='', type=str, help='S3 path to model')
+    parser.add_argument('--image_size_metrics', default=256, type=int,
+                        help='Image size for computing FID, Inception, and CLIP metrics. (default: %(default)s)')
+    parser.add_argument('--name', default='', type=str,
+                        help='wandb and folder name (default: %(default)s)')
+    parser.add_argument('--sr_name', default='', type=str,
+                        help='SR wandb and folder name (default: %(default)s)')
+    parser.add_argument('--output_dir', default='',
+                        help='Path where to save, empty for no saving')
+    parser.add_argument('--num_log_images', default=100,
+                        help='Number of images to log (default: %(default)s)')
+    parser.add_argument('--save_all_outputs', action='store_true',
+                        help='Save all conditioning and target modalities for all drawn samples as individual files.')
+    parser.add_argument('--no_save_all_outputs', action='store_false', dest='save_all_outputs',
+                        help='Do not save any outputs.')
+    parser.set_defaults(save_all_outputs=False)
+    
+    # Wandb logging
+    parser.add_argument('--log_wandb', default=False, action='store_true',
+                        help='Log training and validation metrics to wandb')
+    parser.add_argument('--no_log_wandb', action='store_false', dest='log_wandb')
+    parser.set_defaults(log_wandb=False)
+    parser.add_argument('--wandb_project', default=None, type=str,
+                        help='Project name on wandb')
+    parser.add_argument('--wandb_entity', default=None, type=str,
+                        help='User or team name on wandb')
+    parser.add_argument('--wandb_run_name', default=None, type=str,
+                        help='Run name on wandb')
+    parser.add_argument('--wandb_mode', default='online', type=str,
+                        help='Wandb mode')
+    parser.add_argument('--show_user_warnings', default=False, action='store_true')
+
+    # GPU / Distributed parameters
+    parser.add_argument('--device', default='cuda',
+                        help='Device to use for training / testing')
+    parser.add_argument('--dist_gen', action='store_true', default=False,
+                        help='Enabling distributed generation')
+    parser.add_argument('--no_dist_gen', action='store_false', dest='dist_gen',
+                        help='Disabling distributed generation')
+    parser.set_defaults(dist_gen=True)
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+
+    # Parse config file if there is one
+    args_config, remaining = config_parser.parse_known_args(args)
+
+    if args_config.config:
+        with open(args_config.config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+    if args_config.data_config:
+        with open(args_config.data_config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+    if args_config.gen_config:
+        with open(args_config.gen_config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+    if args_config.sr_config:
+        with open(args_config.sr_config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+
+    #The main arg parser parses the rest of the args, the usual
+    # defaults will have been overridden if config file specified.
+    args = parser.parse_args(remaining)
+
+    # Add the config paths if given
+    args.config_path = args_config.config
+    args.data_config_path = args_config.data_config
+    args.gen_config_path = args_config.gen_config
+    args.sr_config_path = args_config.sr_config
+    
+    return args
